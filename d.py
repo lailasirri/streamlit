8aea00d0e836f2aabffc2af80ee0e839e5eea5bc
yin <97283180+lailasirri@users.noreply.github.com>
2023-02-03 13:23:46 +0800

    Update d.py

--- d.py
+++ d.py
@@ -4,7 +4,7 @@ import numpy as np
 
 option = st.sidebar.selectbox(
     'Silakan pilih:',
-    ('Home','Dataframe','Matrix')
+    ('Home','Dataframe','Matrix','Item-Similarity')
 )
 
 if option == 'Home' or option == '':
@@ -33,4 +33,21 @@ elif option == 'Matrix':
     df_2 = pd.merge(df, agg_ratings_1[['namahotel']], on='namahotel', how='inner')
     matrix = df_2.pivot_table(index='namahotel', columns='userId', values='rating')
     matrix #menampilkan matrix data
+
+elif option == 'Item-Similarity':
+    st.write("""## Similarity""") #menampilkan judul halaman similarity
+
+    ratings=pd.read_csv('rating.csv', sep=';')
+    hotels = pd.read_csv('hotel.csv', sep=';')
+
+    # Keep the hotels with over 1 ratings
+    df = pd.merge(ratings, hotels, on='hotelId', how='inner')
+    agg_ratings = df.groupby('namahotel').agg(mean_rating = ('rating', 'mean'), number_of_ratings = ('rating', 'count')).reset_index()
+    agg_ratings_1 = agg_ratings[agg_ratings['number_of_ratings']>1]
+    agg_ratings_1.sort_values(by='number_of_ratings', ascending=False)
+    df_2 = pd.merge(df, agg_ratings_1[['namahotel']], on='namahotel', how='inner')
+    matrix = df_2.pivot_table(index='namahotel', columns='userId', values='rating')
+    matrix_norm = matrix.subtract(matrix.mean(axis=1), axis = 0)
+    tem_similarity = matrix_norm.T.corr()
+    item_similarity #menampilkan similarity
     
