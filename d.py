1299b94a672569dc191f44829f2856c8f31c1f4d
yin <97283180+lailasirri@users.noreply.github.com>
2023-02-06 09:49:19 +0800

    Update d.py

--- d.py
+++ d.py
@@ -8,11 +8,11 @@ option = st.sidebar.selectbox(
 )
 
 if option == 'Home' or option == '':
-    st.write("""# SISTEM REKOMENDASI HOTEL
-#### Sistem rekomendasi hotel merupakan sistem yang membantu memprediksi rating atau memberikan rekomendasi hotel berdasarkan data rating dan informasi hotel sebelumnya dari pengguna. Sistem ini menggunakan algoritma similarity untuk membandingkan rating hotel dan menemukan hotel yang paling mirip untuk memprediksi rating atau memberikan rekomendasi.""") #menampilkan halaman utama
+    st.write("""# Welcome to Lombok Island Hotel Recommendation System
+Sistem rekomendasi hotel pulau Lombok adalah sistem yang membantu pengguna menemukan hotel yang sesuai dengan kebutuhan dan preferensi mereka. Ini bisa dilakukan dengan menganalisis data seperti rating dan preferensi pengguna sebelumnya, lokasi, harga, dan fasilitas hotel. Pada metode item-based, sistem akan menganalisis item (hotel) yang serupa dan memberikan rekomendasi hotel berdasarkan hotel yang paling serupa dengan hotel yang pernah diterima pengguna. Metode ColLaborative Filtering menggabungkan beberapa metode lain dan memberikan rekomendasi berdasarkan analisis Collaborative dari data pengguna dan item. Dengan demikian, sistem rekomendasi hotel pulau Lombok dapat membantu pengguna menemukan hotel yang sesuai dengan kebutuhan dan preferensi mereka, mempermudah proses pemesanan, dan membantu pengguna menemukan hotel terbaik untuk dikunjungi saat berlibur di pulau Lombok.""") #menampilkan halaman utama
 elif option == 'Dataframe':
     st.write("""## Dataframe""") #menampilkan judul halaman dataframe
-
+    
     #membuat dataframe dengan pandas yang terdiri dari 2 kolom dan 4 baris data
     ratings=pd.read_csv('rating.csv', sep=';')
     hotels = pd.read_csv('hotel.csv', sep=';')
