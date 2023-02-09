4fe062dcc461304729c6dabbe483bae28ed12fd9
yin <97283180+lailasirri@users.noreply.github.com>
2023-02-09 15:00:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ elif option == 'Dataframe':
     st.write("""## Dataframe""") #menampilkan judul halaman dataframe
     
     #membuat dataframe dengan pandas yang terdiri dari 2 kolom dan 4 baris data
-    ratings=pd.read_csv('rating.csv', sep=';')
+    ratings=pd.read_csv('datafix.csv', sep=';')
     rating 
 
 elif option == 'Hotels':
@@ -27,7 +27,7 @@ elif option == 'Recommendation':
 
     st.title('Get Your Preferred Hotel !')
 
-    ratings=pd.read_csv('rating.csv', sep=';')
+    ratings=pd.read_csv('datafix.csv', sep=';')
     # Keep the hotels with over 1 ratings
     # Aggregate by hotel
     agg_ratings = ratings.groupby('namahotel').agg(mean_rating = ('rating', 'mean'), number_of_ratings = ('rating', 'count')).reset_index()
@@ -50,7 +50,7 @@ elif option == 'Recommendation':
 
     if st.button('View Recommendation Result'):
 
-        ratings=pd.read_csv('rating.csv', sep=';')
+        ratings=pd.read_csv('datafix.csv', sep=';')
          # Keep the hotels with over 1 ratings
          # Aggregate by hotel
          agg_ratings = ratings.groupby('namahotel').agg(mean_rating = ('rating', 'mean'), number_of_ratings = ('rating', 'count')).reset_index()
