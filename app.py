c61f54d15084cd15595adc206378e95d5804ae19
yin <97283180+lailasirri@users.noreply.github.com>
2023-02-09 18:53:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -101,10 +101,10 @@ elif option == 'Recommendation':
             picked_userid_rating = pd.DataFrame(matrix_norm[picked_userid].dropna(axis=0, how='all').sort_values(ascending=False)).reset_index().rename(columns={picked_userid:'rating'})
             
             # Check if the user has rated any hotels
-            if picked_userid_rating.empty:
+        if picked_userid_rating.empty:
             # If the user has not rated any hotels, recommend the most popular hotels
                most_popular_hotels = matrix_norm.mean(axis=1).sort_values(ascending=False).index.tolist()
-               return most_popular_hotels[:number_of_recommendations]
+            return most_popular_hotels[:number_of_recommendations]
             
             # Dictionary to save the unrating hoteland predicted rating pair
             rating_prediction ={}  
