7e89e3ccdb33e5dae1792350855882bb021b0c5a
yin <97283180+lailasirri@users.noreply.github.com>
2023-03-16 17:24:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -74,23 +74,15 @@ elif option == 'Recommendation':
         # Item similarity matrix using Pearson correlation
         item_similarity = matrix_norm.T.corr()
 
-        # Pick a user ID
-        picked_userid = userId
         # Pick a hotels
         picked_hotel = namahotel
-        # Hotels that the target user has rating
-        picked_userid_rating = pd.DataFrame(matrix_norm[picked_userid].dropna(axis=0, how='all').sort_values(ascending=False)).reset_index().rename(columns={picked_userid:'rating'})
+     
         # Similarity score hotels
         picked_hotel_similarity_score = item_similarity[[picked_hotel]].reset_index().rename(columns={picked_hotel:'similarity_score'})
         n = 10
-        picked_userid_rating_similarity = pd.merge(left=picked_userid_rating, 
-                                                    right=picked_hotel_similarity_score, 
-                                                    on='namahotel', 
-                                                    how='inner')\
-                                             .sort_values('similarity_score', ascending=False)[:10]
-        predicted_rating = round(np.average(picked_userid_rating_similarity['rating'], 
-                                    weights=picked_userid_rating_similarity['similarity_score']), 6)
-        print(f'The predicted rating for {picked_hotel} by user {picked_userid} is {predicted_rating}' )
+  
+        predicted_rating = round(np.average(picked_hotel_rating_similarity['similarity_score']), 6)
+        print(f'The predicted rating for {picked_hotel} is {predicted_rating}' )
         
 # Item-based recommendation function
 def item_based_rec(picked_hotel, number_of_similar_items=5, number_of_recommendations=5):
